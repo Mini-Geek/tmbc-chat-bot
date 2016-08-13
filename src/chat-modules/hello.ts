1cfc4a143ae274c985e32056d4c4720c83561145
Tim Sorbera <tim.sorbera@gmail.com>
2016-08-13 17:30:40 -0500

    add regex for wider range of hello values

--- src/chat-modules/hello.ts
+++ src/chat-modules/hello.ts
@@ -2,15 +2,14 @@ import fbapi = require("facebook-chat-api");
 import { IChatModule } from "./chat-module";
 
 export class HelloModule implements IChatModule {
+    private pattern: RegExp = /^(Hello|hi),? Robby( A[sz]imov)?[\!\.\?]?$/i;
     public getMessageType(): string { return "message"; }
     public getHelpLine(): string {
         return "Hello, Robby: say hello";
     }
 
     public processMessage(api: fbapi.Api, message: fbapi.MessageEvent): void {
-        if (message.body &&
-            (message.body.toUpperCase() === "Hello, Robby".toUpperCase() ||
-             message.body.toUpperCase() === "Hello Robby".toUpperCase())) {
+        if (message.body && this.pattern.test(message.body)) {
             api.sendMessage("Hello", message.threadID);
         }
     }
