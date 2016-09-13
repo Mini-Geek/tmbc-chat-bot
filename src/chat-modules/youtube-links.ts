dfc44a452027829a4f34901a14e1be61dc7520e1
Tim Sorbera <tim.sorbera@gmail.com>
2016-09-13 12:07:12 -0500

    Also check for YT links in attachment

--- src/chat-modules/youtube-links.ts
+++ src/chat-modules/youtube-links.ts
@@ -3,7 +3,7 @@ import { Utils } from "../utils";
 import { IContext, MessageModule } from "./chat-module";
 
 export class YouTubeLinksModule extends MessageModule {
-    private fbPattern: RegExp = /https?:\/\/l\.facebook\.com\/l\.php\?u=([^&\ ]+)/i;
+    private fbPattern: RegExp = /https?:\/\/(?:l\.|www\.|)facebook\.com\/l\.php\?u=([^&\ ]+)/i;
     // large regex is easiest as one big line
     // tslint:disable-next-line:max-line-length
     private ytPattern: RegExp = /(?:https?:\/\/|\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))([\w-]{11})(?![\w-])\/?(?:[?&]t=([0-9hms]+))?/i;
@@ -13,6 +13,13 @@ export class YouTubeLinksModule extends MessageModule {
 
     public processMessage(ctx: IContext<fbapi.MessageEvent>): void {
         let msg = ctx.message.body;
+        if (ctx.message.attachments && ctx.message.attachments.length > 0) {
+            let atch = ctx.message.attachments[0];
+            let url = atch.url || (atch as fbapi.ShareAttachment).facebookUrl;
+            if (url) {
+                msg = url;
+            }
+        }
         if (msg) {
             let fbMatches = this.fbPattern.exec(msg);
             if (fbMatches) {
