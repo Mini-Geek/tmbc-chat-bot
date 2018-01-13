84badb8bead1d6e664bf390f79a870b2b5123c5a
Tim Sorbera <tim.sorbera@gmail.com>
2018-01-12 21:45:21 -0600

    modify bit.ly link to satisy FB's link security concerns

--- src/chat-modules/links.ts
+++ src/chat-modules/links.ts
@@ -10,8 +10,8 @@ export class LinksModule extends MessageModule {
     public processMessage(ctx: IContext<fbapi.MessageEvent>): void {
         if (ctx.message.body === "/links") {
             Utils.sendMessage(ctx,
-                "https://bit.ly/TMBCChatGuide" + "\n" +
-                "https://www.blimeycow.com/cowmoonity" + "\n" + "\n" +
+                "https://bit.ly/TmbcChatGuide" + "\n" +
+                "https://www.blimeycow.com/cowmoonity" + "\n\n" +
                 "https://github.com/Mini-Geek/tmbc-chat-bot" + "\n" +
                 "https://bit.ly/TMBC-archive-2016-12-09");
         }
