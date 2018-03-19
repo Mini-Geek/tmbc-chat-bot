eeb5550e4fd72e81437538ee8ba51d4b17503040
Tim Sorbera <tim.sorbera@gmail.com>
2018-03-18 19:38:17 -0500

    change ily sayings

--- src/chat-modules/love.ts
+++ src/chat-modules/love.ts
@@ -5,16 +5,18 @@ import { IContext, MessageModule } from "./chat-module";
 export class LoveModule extends MessageModule {
     private ilyPattern: RegExp = new RegExp("^I love (y'?a'?ll|you(|s|se))\\b", "i");
     private petNames: string[] = [
+        "010010110100100101001100010011000010000001000001010011000100" +
+        "110000100000010010000101010101001101010000010100111001010011",
         "babe-honey",
-        "baby",
-        "cutie",
+        "because your neck is like the tower of David, built in rows of stone",
         "darlin'",
+        "good lookin'",
         "hon-baby",
-        "honey",
         "honey bun",
         "infant human",
-        "lover",
-        "sweetie",
+        "my beloved",
+        "my lily among brambles",
+        "my sister*, my bride**\n*or brother\n**or bridegroom",
         "sweet thang",
     ];
     public getHelpLine(): string {
