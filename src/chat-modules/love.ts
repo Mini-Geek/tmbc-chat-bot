103cc620adcc8b86faefbceaee2dde42a4c2b077
Tim Sorbera <tim.sorbera@gmail.com>
2018-04-06 07:46:47 -0500

    change LoveModule to be random

--- src/chat-modules/love.ts
+++ src/chat-modules/love.ts
@@ -25,29 +25,8 @@ export class LoveModule extends MessageModule {
 
     public processMessage(ctx: IContext<fbapi.MessageEvent>): void {
         if (ctx.message.body && this.ilyPattern.test(ctx.message.body)) {
-            const userHash = this.hash(ctx.message.senderID);
-            const name = this.petNames[userHash % this.petNames.length];
+            const name = this.petNames[Math.floor(Math.random() * this.petNames.length)];
             Utils.sendMessage(ctx, `I love you too, ${name}`);
         }
     }
-
-    private hash(str: string): number {
-        let hash = 0;
-        let i: number;
-        let chr: number;
-        if (str.length === 0) {
-            return hash;
-        }
-        /* tslint:disable:no-bitwise
-           taken from https://stackoverflow.com/a/7616484/781792 */
-        for (i = 0; i < str.length; i++) {
-          chr   = str.charCodeAt(i);
-          hash  = ((hash << 5) - hash) + chr;
-          hash |= 0; // Convert to 32bit integer
-        }
-        if (hash < 0) {
-            hash = -hash;
-        }
-        return hash;
-      }
 }
