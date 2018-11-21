2c3ef83d2e90c2b922888e2bf68692a4db4d9172
Tim Sorbera <tim.sorbera@gmail.com>
2018-11-20 23:04:48 -0600

    make /die and /kill mark message handled

--- src/chat-modules/die.ts
+++ src/chat-modules/die.ts
@@ -10,6 +10,7 @@ export class DieModule extends MessageModule {
 
     public processMessage(ctx: IContext<fbapi.MessageEvent>): void {
         if (ctx.message.body === "/die" || ctx.message.body === "/kill") {
+            ctx.messageHandled = true;
             winston.warn("Shutting down due to command", ctx.message);
             ctx.shutdown(ctx.message.body + " command sent, shutting down");
         }
